0ef7ad3b361f4c44e31cd0ec1a9da23fd2bdc6b2
Tigran Asriyan <tigran.asriyan@gmail.com>
2021-02-20 09:29:33 +0400

    add user cofirmation resource

--- resources/user.py
+++ resources/user.py
@@ -1,6 +1,5 @@
 from flask_restful import Resource
 from flask import request
-from marshmallow import ValidationError
 from werkzeug.security import safe_str_cmp
 from flask_jwt_extended import (
     create_access_token,
@@ -20,6 +19,8 @@ USER_NOT_FOUND = "User not found."
 USER_DELETED = "User deleted."
 INVALID_CREDENTIALS = "Invalid credentials!"
 USER_LOGGED_OUT = "User <id={}> successfully logged out."
+NOT_CONFIRMED_ERROR = "You have not confirmed registration, please check your email <{}>."
+USER_CONFIRMED = "User confirmed."
 
 user_schema = UserSchema()
 
@@ -63,10 +64,11 @@ class UserLogin(Resource):
 
         # this is what the `authenticate()` function did in security.py
         if user and safe_str_cmp(user.password, user_data["password"]):
-            # identity= is what the identity() function did in security.py—now stored in the JWT
-            access_token = create_access_token(identity=user.id, fresh=True)
-            refresh_token = create_refresh_token(user.id)
-            return {"access_token": access_token, "refresh_token": refresh_token}, 200
+            if user.activated:
+                access_token = create_access_token(identity=user.id, fresh=True)
+                refresh_token = create_refresh_token(user.id)
+                return {"access_token": access_token, "refresh_token": refresh_token}, 200
+            return {"message": NOT_CONFIRMED_ERROR.format(user.username)}, 400
 
         return {"message": INVALID_CREDENTIALS}, 401
 
@@ -88,3 +90,14 @@ class TokenRefresh(Resource):
         current_user = get_jwt_identity()
         new_token = create_access_token(identity=current_user, fresh=False)
         return {"access_token": new_token}, 200
+
+class UserConfirm(Resource):
+    @classmethod
+    def get(cls, user_id: int):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {"message": USER_NOT_FOUND}, 404
+
+        user.activated = True
+        user.save_to_db()
+        return {"message": USER_CONFIRMED}, 200
